bab62f79f39d19b89a03c594051b56638754dc78
Cool-Pan <ivlioioilvi@gmail.com>
2019-07-11 09:59:54 +0800

    修复: 日常维护包列表

---
+++ pkg/i3bra.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# 安装 i3bra
+#
+
+# 创建临时目录
+mkdir -p "$HOME/.cache/pkg-tmp"
+
+# 下载包
+git clone --depth=1 https://github.com/Cool-Pan/i3bra "$HOME/.config/Manual/i3bra"
+
+# 安装包
+mkdir -p "$HOME/.config/i3bra"
+cp -rf "$HOME/.config/Manual/i3bra/template/config.template" "$HOME/.config/i3bra/config"
+ln -sf "$HOME/.config/Manual/i3bra/i3bra"                    "$HOME/.config/Manual/bin/i3bra"
+
+# 赋予执行权限
+chmod +x "$HOME/.config/Manual/bin/i3bra"
+
+# 清理环境
+rm -rf "$HOME/.cache/pkg-tmp"
+sync
--- pkg/tmp/minecraft.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -L "http://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar" -O
-mv -f Minecraft.jar ~/.config/lib
