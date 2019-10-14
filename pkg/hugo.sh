ea9dddf7b314d8ef52f123a5b0e3a1ce792e9aad
Cool-Pan <ivlioioilvi@gmail.com>
2019-10-15 00:51:30 +0800

    Errare humanum est.

--- pkg/hugo.sh
+++ pkg/hugo.sh
@@ -9,12 +9,14 @@ mkdir -p "$HOME/.cache/pkg-tmp"
 
 # 下载包
 cd "$HOME/.cache/pkg-tmp"
-curl -L "https://github.com/gohugoio/hugo/releases/download/v0.55.6/hugo_0.55.6_Linux-64bit.tar.gz" -O
+#curl -L "https://github.com/gohugoio/hugo/releases/download/v0.55.6/hugo_0.55.6_Linux-64bit.tar.gz" -O
+curl -L "https://github.com/gohugoio/hugo/releases/download/v0.56.0/hugo_0.56.0_Linux-64bit.tar.gz" -O
 cd ~
 
 # 安装包
 cd "$HOME/.cache/pkg-tmp"
-mkdir hugo && tar -xzvf hugo_0.55.6_Linux-64bit.tar.gz -C hugo
+#mkdir hugo && tar -xzvf hugo_0.55.6_Linux-64bit.tar.gz -C hugo
+mkdir hugo && tar -xzvf hugo_0.56.0_Linux-64bit.tar.gz -C hugo
 mv -f "hugo" "$HOME/.config/Manual/"
 ln -sf "$HOME/.config/Manual/hugo/hugo" "$HOME/.config/Manual/ubin/hugo"
 cd ~
